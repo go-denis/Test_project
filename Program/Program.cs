c63547f5c069e338684a5059e4404d9e99209299
Delfin-Go <go-denis@mail.ru>
2022-12-28 00:55:05 +0300

    Первая функция

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] array_one = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array_second = new string[array_one.Length];
+
+void ArraySecond(string[] array_one, string[] array_second)
+{
+    int count = 0;
+    for (int i = 0; i < array_one.Length; i++)
+    {
+    if(array_one[i].Length <= 3)
+        {
+        array_second[count] = array_one[i];
+        count++;
+        }
+    }
+}
